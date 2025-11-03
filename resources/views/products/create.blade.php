53aeccbd6b7d0443efdb492002829ec475eb5c15
KumoRohan <rohan@kumo-labs.com>
2025-11-03 15:37:02 +0545

    test and init

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Add Product</title>
+</head>
+<body>
+
+    <h1>Add New Product</h1>
+
+    <form action="{{ route('products.store') }}" method="POST">
+        @csrf
+
+        <label>Name:</label><br>
+        <input type="text" name="name" required><br><br>
+
+        <label>Price (Rs):</label><br>
+        <input type="number" name="price" required><br><br>
+
+        <button type="submit">Save</button>
+    </form>
+
+    <br>
+    <a href="{{ route('products.index') }}">Back</a>
+
+</body>
+</html>
